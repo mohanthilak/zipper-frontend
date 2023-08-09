81937ef6d114435e64f2381311bba2b238094d13
rafipopz98 <itsrealrafi@gmail.com>
2023-08-10 02:30:19 +0530

    login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,12 +1,42 @@
-import Navbar from "../navbar/page"
-
+import Navbar from "../navbar/page";
+import "./Login.css";
 const Login = () => {
   return (
-    <div>
-    <Navbar />
-        login
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="login">
+        <div className="login_container">
+          <h3>Sign in to zipper</h3>
+          <button  className="login-with-google-btn">
+            Sign in with Google
+          </button>
+          <div className="email_signin">
+          <span>or signin with email</span>
+          </div>
+          <form >
+            <div className="login_input">
+              <label className="gg" htmlFor="">Username or email</label><br />
+              <input type="text" /> 
+            </div>
+            <div className="login_input">
+              <label htmlFor="">Password</label><br />
+              <input type="text" /> <br />
+              <p>
 
-export default Login
+              forgot Password !!
+              </p>
+            </div>
+            <div className="signin_button">
+              SIGN IN
+            </div>
+          </form>
+          <div className="signup_to">
+          Don’t have an account? Sign up
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
