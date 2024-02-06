58ce0f1fba8a7319d902bc50e3e39a12abe18d63
mohanthilak <mohan.thilak410@gmail.com>
2024-02-07 01:09:23 +0530

    path

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 let baseURL = "http://localhost:4000";
+console.log("\n!@!!!@!@!@", {env: process.env.NODE_ENV})
 if(process.env.NODE_ENV === "production"){
   baseURL = process.env.BasePath_prod;
 }
 export default axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL,
   Headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
